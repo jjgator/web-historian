76c9545ed3139aead95d9772941d4bae8d84714a
Hack Reactor Student <student@hackreactor.com>
2017-09-09 12:32:37 -0500

    Promisified

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
 var axios = require('axios');
+var Promise = require('bluebird');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -26,32 +27,88 @@ exports.initialize = function(pathsObj) {
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function(callback) {
-  fs.readFile(exports.paths.list, 'utf8', function(err, data) {
-    callback(data.split('\n'));
-  });
-};
+// exports.readListOfUrls = function(callback) {
+//   fs.readFile(exports.paths.list, 'utf8', function(err, data) {
+//     callback(data.split('\n'));
+//   });
+// };
 
-exports.isUrlInList = function(url, callback) {
-  exports.readListOfUrls(function(array) {
-    callback(array.includes(url));
-  });
-};
-
-exports.addUrlToList = function(url, callback) {
-  fs.appendFile(exports.paths.list, url + '\n', callback);
-};
-
-exports.isUrlArchived = function(url, callback) {
-  callback(fs.existsSync(path.join(exports.paths.archivedSites, url)));
-};
-
-exports.downloadUrls = function(urls) {
-  urls.forEach(function(url) {
-    axios.get('http://' + url).then(function(response) {
-      fs.writeFileSync(path.join(exports.paths.archivedSites, url), response.data, 'utf8');
-    }).catch(function(error) {
-      console.log(error);
+exports.readListOfUrls = function() {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(exports.paths.list, 'utf8', function(err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data.split('\n'));
+      }
     });
   });
 };
+
+// exports.isUrlInList = function(url, callback) {
+//   exports.readListOfUrls(function(array) {
+//     callback(array.includes(url));
+//   });
+// };
+
+exports.isUrlInList = function(url) {
+  return new Promise(function(resolve, reject) {
+    exports.readListOfUrls()
+    .then(function(array) {
+      resolve(array.includes(url));
+    });
+  });
+};
+
+// exports.addUrlToList = function(url, callback) {
+//   fs.appendFile(exports.paths.list, url + '\n', callback);
+// };
+
+exports.addUrlToList = function(url) {
+  return new Promise(function(resolve, reject) {
+    fs.appendFile(exports.paths.list, url + '\n', function(err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
+// exports.isUrlArchived = function(url, callback) {
+//   callback(fs.existsSync(path.join(exports.paths.archivedSites, url)));
+// };
+
+exports.isUrlArchived = function(url) {
+  return new Promise(function(resolve, reject) {
+    resolve(fs.existsSync(path.join(exports.paths.archivedSites, url)));
+  });
+};
+
+// exports.downloadUrls = function(urls) {
+//   urls.forEach(function(url) {
+//     axios.get('http://' + url).then(function(response) {
+//       fs.writeFileSync(path.join(exports.paths.archivedSites, url), response.data, 'utf8');
+//     }).catch(function(error) {
+//       console.log(error);
+//     });
+//   });
+// };
+
+exports.downloadUrls = function(urls) {
+  return new Promise(function(resolve, reject) {
+    urls.forEach(function(url) {
+      axios.get('http://' + url).then(function(response) {
+        fs.writeFileSync(path.join(exports.paths.archivedSites, url), response.data, 'utf8');
+      }).catch(function(error) {
+        reject(error);
+      });
+    });
+    resolve();
+  });
+};
+
+
+
+
